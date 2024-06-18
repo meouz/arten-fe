defa6b5e3fe1b239404f6f38795149249b8cd7a5
meouz <samuel.deanova@gmail.com>
2024-06-18 15:24:19 +0700

    feat: navigate to TranslateActivity

--- app/src/main/java/com/example/arten/MainActivity.kt
+++ app/src/main/java/com/example/arten/MainActivity.kt
@@ -1,20 +1,18 @@
 package com.example.arten
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.arten.ui.translate.TranslateActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        finish()
+        val intent = Intent(this, TranslateActivity::class.java)
+        startActivity(intent)
     }
 }
