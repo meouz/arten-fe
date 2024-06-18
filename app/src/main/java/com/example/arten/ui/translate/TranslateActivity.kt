ced34827982a23882f4a369b436e5cd215cdd9e4
meouz <samuel.deanova@gmail.com>
2024-06-18 15:25:12 +0700

    feat: Add TranslateActivity

---
+++ app/src/main/java/com/example/arten/ui/translate/TranslateActivity.kt
@@ -0,0 +1,15 @@
+package com.example.arten.ui.translate
+
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.example.arten.R
+
+class TranslateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_translate)
+    }
+}
