aaf239790572cb943b752f79c5699e99684e8279
meouz <samuel.deanova@gmail.com>
2024-06-19 18:49:34 +0700

    feat: Add decide permission
    user can decide whether give permission for using audio or not

--- app/src/main/java/com/example/arten/ui/translate/TranslateActivity.kt
+++ app/src/main/java/com/example/arten/ui/translate/TranslateActivity.kt
@@ -1,15 +1,52 @@
 package com.example.arten.ui.translate
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.media.MediaRecorder
 import android.os.Bundle
-import android.view.Window
-import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
+import androidx.core.app.ActivityCompat
 import com.example.arten.R
 
+const val REQUEST_CODE = 200
+
 class TranslateActivity : AppCompatActivity() {
+    
+    companion object {
+        private var permissionGranted = false
+    }
+    
+    private var permissions = arrayOf(Manifest.permission.RECORD_AUDIO)
+    private lateinit var recorder: MediaRecorder
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_translate)
+        
+        permissionGranted = ActivityCompat.checkSelfPermission(this, permissions[0]) ==
+                PackageManager.PERMISSION_GRANTED
+        
+        if (!permissionGranted)
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE)
+    }
+    
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray,
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        
+        if (requestCode == REQUEST_CODE)
+            permissionGranted = grantResults[0] == PackageManager.PERMISSION_GRANTED
+    }
+    
+    fun startRecording() {
+        if (!permissionGranted) {
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE)
+            println("No permission granted")
+            return
+        }
+        // start recording
     }
 }
