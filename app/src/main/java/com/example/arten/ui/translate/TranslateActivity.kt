13861a5b7a4c89f62cd62867f89642cd13c7704a
meouz <samuel.deanova@gmail.com>
2024-06-19 18:57:47 +0700

    feat: Add record audio
    let the user record audio and prepare a file storage location

--- app/src/main/java/com/example/arten/ui/translate/TranslateActivity.kt
+++ app/src/main/java/com/example/arten/ui/translate/TranslateActivity.kt
@@ -7,6 +7,10 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.example.arten.R
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 const val REQUEST_CODE = 200
 
@@ -14,14 +18,18 @@ class TranslateActivity : AppCompatActivity() {
     
     companion object {
         private var permissionGranted = false
+        private var dirPath = ""
     }
     
     private var permissions = arrayOf(Manifest.permission.RECORD_AUDIO)
+    private lateinit var dirPathMediaRecorder: String
+    private lateinit var filename: String
     private lateinit var recorder: MediaRecorder
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_translate)
+        dirPath = getExternalFilesDir(null)?.absolutePath.toString()
         
         permissionGranted = ActivityCompat.checkSelfPermission(this, permissions[0]) ==
                 PackageManager.PERMISSION_GRANTED
@@ -47,6 +55,46 @@ class TranslateActivity : AppCompatActivity() {
             println("No permission granted")
             return
         }
+        println("Start recording")
         // start recording
+        
+        dirPathMediaRecorder = "$dirPath/"
+        val file = File(dirPathMediaRecorder)
+        if (!file.exists()) {
+            file.mkdirs()
+            println("create directory")
+        }
+        
+        val dateFormat = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault())
+        filename = "audio_${dateFormat.format(Date())}"
+        println("File will be saved in: $dirPathMediaRecorder/$filename.mp3")
+        
+        recorder = MediaRecorder()
+        
+        println("set audio source")
+        recorder.apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
+            setOutputFile("$dirPathMediaRecorder/$filename.mp3")
+            
+            try {
+                prepare()
+                start()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+        println("success")
+    }
+    
+    fun pauseRecording() {
+        recorder.pause()
+        println("success")
+    }
+    
+    fun resumeRecording() {
+        recorder.resume()
+        println("success")
     }
 }
--- app/src/main/java/com/example/arten/ui/translate/TranslateFragment.kt
+++ app/src/main/java/com/example/arten/ui/translate/TranslateFragment.kt
@@ -6,19 +6,51 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.arten.R
 import com.example.arten.adapter.RVTranslateAdapter
 import com.example.arten.databinding.FragmentTranslateBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class TranslateFragment : Fragment() {
     
     private var _binding: FragmentTranslateBinding? = null
     private val binding get() = _binding!!
+    private val translateActivity: TranslateActivity = TranslateActivity()
+    private var isRecording = false
+    private var isPaused = false
     
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
         _binding = FragmentTranslateBinding.inflate(inflater, container, false)
+        
+        val fabRecord: FloatingActionButton = binding.fabRecord
+        
+        fabRecord.setOnClickListener {
+            when {
+                isPaused -> {
+                    isPaused = false
+                    isRecording = true
+                    translateActivity.resumeRecording()
+                    fabRecord.setImageResource(R.drawable.ic_resume)
+                }
+                
+                isRecording -> {
+                    isPaused = true
+                    isRecording = false
+                    translateActivity.pauseRecording()
+                    fabRecord.setImageResource(R.drawable.ic_pause)
+                }
+                
+                else -> {
+                    isRecording = true
+                    translateActivity.startRecording()
+                    fabRecord.setImageResource(R.drawable.ic_resume)
+                }
+            }
+        }
+        
         return binding.root
     }
     
