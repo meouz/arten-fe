cad708c7f043b7c28efe04e3aac755d863e8d533
meouz <samuel.deanova@gmail.com>
2024-06-27 20:58:20 +0700

    docs: remove unused import

--- app/src/main/java/com/example/arten/ui/translate/components/RecordFragment.kt
+++ app/src/main/java/com/example/arten/ui/translate/components/RecordFragment.kt
@@ -1,14 +1,11 @@
 package com.example.arten.ui.translate.components
 
-import android.content.Context
 import android.os.Bundle
-import android.os.VibrationEffect
-import android.os.Vibrator
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.example.arten.R
 import com.example.arten.databinding.FragmentRecordBinding
 import com.example.arten.ui.translate.TranslateActivity
@@ -25,8 +22,7 @@ class RecordFragment : Fragment(), Timer.OnTimerTickListener {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
+    ): View {
         binding = FragmentRecordBinding.inflate(inflater, container, false)
         
         timer = Timer(this)
