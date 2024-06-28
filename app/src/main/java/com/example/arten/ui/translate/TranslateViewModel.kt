30af05dbc6e2fae0053c9b82de6571c60ce3145b
meouz <samuel.deanova@gmail.com>
2024-06-28 16:03:41 +0700

    feat: create ViewModel for Translate

---
+++ app/src/main/java/com/example/arten/ui/translate/TranslateViewModel.kt
@@ -0,0 +1,110 @@
+package com.example.arten.ui.translate
+
+import android.media.MediaRecorder
+import androidx.lifecycle.ViewModel
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+class TranslateViewModel : ViewModel() {
+    companion object {
+        lateinit var dirPathMediaRecorder: String
+    }
+    
+    var isRecording = false
+    var isPaused = false
+    var permissionGranted = false
+    private lateinit var filename: String
+    private lateinit var recorder: MediaRecorder
+    var language: String = "English"
+    var languageResult: String = "Indonesia"
+    
+    private fun prepareRecorder() {
+        val file = File(dirPathMediaRecorder)
+        if (!file.exists())
+            file.mkdirs()
+        
+        val dateFormat = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault())
+        filename = "audio_${dateFormat.format(Date())}"
+        println("File will be saved in: $dirPathMediaRecorder/$filename.mp3")
+        
+        recorder = MediaRecorder()
+        
+        recorder.apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
+            setOutputFile("$dirPathMediaRecorder/$filename.mp3")
+            
+            try {
+                prepare()
+                start()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+    
+    fun startRecording() {
+        when {
+            isPaused -> {
+                resumeRecording()
+                isPaused = false
+            }
+            isRecording -> {
+                pauseRecording()
+                isPaused = true
+            }
+            else -> {
+                try {
+                    prepareRecorder()
+                    isRecording = true
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
+    
+    fun resetRecording() {
+        try {
+            isRecording = false
+            isPaused = false
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+    
+    fun sendRecord() {
+        try {
+            isRecording = false
+            isPaused = false
+            stopRecording()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+    
+    private fun stopRecording() {
+        recorder.stop()
+    }
+    
+    private fun pauseRecording() {
+        recorder.pause()
+    }
+    
+    private fun resumeRecording() {
+        recorder.resume()
+    }
+    
+    fun switchLanguage() {
+        val temp: String = this.language
+        this.language = this.languageResult
+        this.languageResult = temp
+    }
+    
+    fun setDirPathMediaRecorder(dirPath: String) {
+        dirPathMediaRecorder = dirPath
+    }
+}
