f8e6b3b6d5dbc809145f033aa2a2086ef53bd0e5
meouz <samuel.deanova@gmail.com>
2024-08-01 19:15:30 +0700

    chore

--- app/src/main/java/com/example/arten/ui/translate/TranslateActivity.kt
+++ app/src/main/java/com/example/arten/ui/translate/TranslateActivity.kt
@@ -3,19 +3,13 @@ package com.example.arten.ui.translate
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import androidx.fragment.app.FragmentContainerView
 import com.example.arten.R
 
 const val REQUEST_CODE = 200
 
 class TranslateActivity : AppCompatActivity() {
-    
-    companion object {
-        private lateinit var fragmentCView: FragmentContainerView
-    }
     
     private lateinit var activity: TranslateActivity
     private val viewModel: TranslateViewModel = TranslateViewModel()
@@ -28,7 +22,6 @@ class TranslateActivity : AppCompatActivity() {
         activity = this
         
         viewModel.setDirPathMediaRecorder(getExternalFilesDir(null)?.absolutePath.toString())
-        fragmentCView = findViewById(R.id.fragmentContainerView2)
         
         viewModel.permissionGranted =
             ActivityCompat.checkSelfPermission(activity, permissions[0]) ==
@@ -47,13 +40,5 @@ class TranslateActivity : AppCompatActivity() {
         
         if (requestCode == REQUEST_CODE)
             viewModel.permissionGranted = grantResults[0] == PackageManager.PERMISSION_GRANTED
-    }
-    
-    fun appear() {
-        fragmentCView.visibility = View.VISIBLE
-    }
-    
-    fun disappear() {
-        fragmentCView.visibility = View.GONE
     }
 }
--- app/src/main/java/com/example/arten/ui/translate/TranslateFragment.kt
+++ app/src/main/java/com/example/arten/ui/translate/TranslateFragment.kt
@@ -5,17 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.arten.adapter.RVTranslateAdapter
 import com.example.arten.databinding.FragmentTranslateBinding
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class TranslateFragment : Fragment() {
     
     private var _binding: FragmentTranslateBinding? = null
     private val binding get() = _binding!!
-    private val translateActivity: TranslateActivity = TranslateActivity()
-    private var appearance: Boolean = false
     
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,34 +18,9 @@ class TranslateFragment : Fragment() {
     ): View {
         _binding = FragmentTranslateBinding.inflate(inflater, container, false)
         
-        val fabRecord: FloatingActionButton = binding.fabRecord
-        fabRecord.setOnClickListener {
-            if (appearance)
-                translateActivity.disappear()
-            else
-                translateActivity.appear()
-            appearance = !appearance
-        }
+        binding.tvTranslate.text = "Translate"
         
         return binding.root
-    }
-    
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        
-        val dummy = listOf(
-            "January", "February", "March", "April", "May", "June", "July", "August", "September",
-            "October", "November", "December",
-            "January", "February", "March", "April", "May", "June", "July", "August", "September",
-            "October", "November", "December",
-        ).toList()
-        
-        val customAdapter = RVTranslateAdapter(dummy)
-        
-        with(binding.recyclerView) {
-            layoutManager = LinearLayoutManager(context)
-            adapter = customAdapter
-        }
     }
     
     override fun onDestroyView() {
