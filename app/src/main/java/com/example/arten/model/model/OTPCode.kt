6958cb8d73c32f34ca79a26f67cfb1d11254f903
meouz <samuel.deanova@gmail.com>
2024-08-01 19:16:57 +0700

    feat: add data class for otp and user

---
+++ app/src/main/java/com/example/arten/model/model/OTPCode.kt
@@ -0,0 +1,3 @@
+package com.example.arten.model.model
+
+data class OTPCode(val code: Int)
---
+++ app/src/main/java/com/example/arten/model/model/User.kt
@@ -0,0 +1,3 @@
+package com.example.arten.model.model
+
+data class User(val username: String, val password: String, val email: String, val token: String)
