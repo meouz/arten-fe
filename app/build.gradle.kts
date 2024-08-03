eccb3725c007ff832cc1bda9afbcad4bb7f1ca31
meouz <samuel.deanova@gmail.com>
2024-08-03 20:04:51 +0700

    chore: implement retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,4 +58,8 @@ dependencies {
     
     // OKHTTP3
     implementation(libs.okhttp)
+    
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
 }
